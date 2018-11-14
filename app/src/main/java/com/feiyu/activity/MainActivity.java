01240ed57b6d1451a4ae051c94b465bd22871ede
CHENGang1001 <cumtcg@126.com>
2018-11-14 21:37:52 +0800

    add:(注册、登陆)：添加界面布局

--- app/src/main/java/com/feiyu/activity/MainActivity.java
+++ app/src/main/java/com/feiyu/activity/MainActivity.java
@@ -1,15 +1,16 @@
 package com.feiyu.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.feiyu.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
